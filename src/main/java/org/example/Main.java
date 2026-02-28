b716ea33d4dbe929003bf96aa366e9ee68a6ae22
EmanuelFuneS <EmanuelFuneS>
2026-02-28 01:27:14 -0300

    feat: added methods in repository employees and test findAll

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.example.model.Employee;
+import org.example.repository.EmployeeRepository;
+import org.example.repository.Repository;
 import org.example.util.DatabaseConnection;
 
 import java.sql.*;
@@ -7,17 +10,11 @@ import java.sql.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         try (Connection myConn = DatabaseConnection.getInstance(); Statement myStamt = myConn.createStatement(); ResultSet myRes = myStamt.executeQuery("SELECT * FROM employees");) {
-            System.out.println("Connection Success");
+            Repository<Employee> repository = new EmployeeRepository();
 
-            while (myRes.next()) {
-
-                System.out.println(myRes.getString("first_name") + " , " + myRes.getString("email"));
-            }
-        } catch (SQLException e) {
-            System.out.println("Connection Failed");
-            throw new RuntimeException(e);
+            repository.findAll().forEach(System.out::println);
         }
     }
 }
